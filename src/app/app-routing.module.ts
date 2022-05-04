13c9bd2af883404d46c9103a5680b7e07d2a4fc1
Aleksander Väliaho <13369279+avaliaho@users.noreply.github.com>
2022-05-04 11:26:49 +0300

    yksityiskohta reititetty, service haeKysymys

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EtusivuComponent } from './etusivu/etusivu.component';
 import { HakuComponent } from './haku/haku.component';
+import { YksityiskohtaComponent } from './yksityiskohta/yksityiskohta.component';
 
 const reitit: Routes = [
   { path: 'questions/:page/:orderby/:order', component: EtusivuComponent },
   { path: 'search/:term/:page', component: HakuComponent },
+  { path: 'detail/:id', component: YksityiskohtaComponent },
   { path: '', redirectTo: '/questions/1/date/desc', pathMatch: 'full' }
 ];
 
